9c8a4f131df7e0c4fe0a3138dd9d2bce8afd0b8a
Dan Kaftan <41203004+DanKaftan@users.noreply.github.com>
2023-06-20 12:45:34 +0300

    Working Forward and Backward Buttons and a direction scroller

--- app/src/main/java/com/example/boatprojectcontrollerapp/MainActivity.java
+++ app/src/main/java/com/example/boatprojectcontrollerapp/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.boatprojectcontrollerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.os.Bundle;
-
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -17,45 +19,99 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     // ESP32 IP address
-    private static final String ESP32_IP = "192.168.246.6";
+    private static final String ESP32_IP = "192.168.99.6";
 
     // ESP32 API endpoint
     private static final String ESP32_API = "http://" + ESP32_IP + "/motor";
 
     // Motor control buttons
-    private Button btnMotorOn;
-    private Button btnMotorOff;
+    private Button btnForward;
+    private Button btnBackward;
+    private SeekBar directionSeekBar;
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Initialize motor control buttons
-        btnMotorOn = findViewById(R.id.btnMotorOn);
-        btnMotorOff = findViewById(R.id.btnMotorOff);
+        btnForward = findViewById(R.id.forwardButton);
+        btnBackward = findViewById(R.id.backwardButton);
+        directionSeekBar = findViewById(R.id.directionSeekBar);
 
-        // Set click listeners for motor control buttons
-        btnMotorOn.setOnClickListener(new View.OnClickListener() {
+
+        btnForward.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                sendMotorCommand("on");
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    System.out.println("Forward Button Pressed");
+                    sendMotorCommand("forward");
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    System.out.println("Forward Button Released");
+                    sendMotorCommand("stop");
+
+                }
+
+                return true;
             }
         });
 
-        btnMotorOff.setOnClickListener(new View.OnClickListener() {
+        btnBackward.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                sendMotorCommand("off");
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    System.out.println("Backward Button Pressed");
+                    sendMotorCommand("backward");
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    System.out.println("Backward Button Released");
+                    sendMotorCommand("stop");
+
+                }
+
+                return true;
             }
         });
+
+        // Update the SeekBar listener
+        directionSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                // Get servo degree from SeekBar progress
+                //int servoDegree = progress;
+
+                // Send servo degree to ESP32
+                sendServoDegree(getServoDegree());
+                System.out.println("servo degree of " + getServoDegree()  + " has been sent to esp32");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Not used
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Not used
+            }
+        });
+
+    }
+    private int getServoDegree() {
+        // Adjust this calculation based on your specific servo range and SeekBar setup
+        return (int) (directionSeekBar.getProgress() * 1.8);
     }
 
     private void sendMotorCommand(String state) {
         String commandUrl = ESP32_API + "?state=" + state;
 
         // Execute the AsyncTask to send the motor command
+        new SendCommandTask().execute(commandUrl);
+    }
+
+    private void sendServoDegree(int degree) {
+        String commandUrl = ESP32_API + "?state=" + Integer.toString(degree);
         new SendCommandTask().execute(commandUrl);
     }
 
@@ -95,3 +151,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+
+
